84e4d09f909088ff65eace0239bcebd72c45cbd1
TiverT <jonasthebrinkmann@gmail.com>
2025-06-04 19:12:56 +0200

    Wrapped some stuff together

--- MyMoney.Core/Models/AccountDashboardDisplayItem.cs
+++ MyMoney.Core/Models/AccountDashboardDisplayItem.cs
@@ -10,24 +10,28 @@ namespace MyMoney.Core.Models
     public partial class AccountDashboardDisplayItem : ObservableObject
     {
         [ObservableProperty]
-        private string _accountName = "";
+        private string _accountName = string.Empty;
 
         [ObservableProperty]
         private Currency _total = new Currency(0m);
 
         public static AccountDashboardDisplayItem FromAccount(Account account)
         {
-            var displayItem = new AccountDashboardDisplayItem();
-            displayItem.AccountName = account.AccountName;
-            displayItem.Total = account.Total;
+            AccountDashboardDisplayItem displayItem = new()
+            {
+                AccountName = account.AccountName,
+                Total = account.Total
+            };
             return displayItem;
         }
 
         public static AccountDashboardDisplayItem FromInitializers(string accountName, Currency total)
         {
-            var displayItem = new AccountDashboardDisplayItem();
-            displayItem.AccountName = accountName;
-            displayItem.Total = total;
+            AccountDashboardDisplayItem displayItem = new()
+            {
+                AccountName = accountName,
+                Total = total
+            };
             return displayItem;
         }
     }
